c0d123adc2eb32836253f9d3bfea7b0b0bd988a9
modernh <36088452+modernh@users.noreply.github.com>
2018-02-02 21:50:47 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,7 +1,7 @@
 tdcli = dofile('./tg/tdcli.lua')
 serpent = (loadfile "./libs/serpent.lua")()
 feedparser = (loadfile "./libs/feedparser.lua")()
-our_id = 167419742 -- Put Here Your Bot ID
+our_id = 545011896-- Put Here Your Bot ID
 --ایدی رباتتونو اینجا بزارید
 URL = require "socket.url"
 http = require "socket.http"
@@ -84,7 +84,7 @@ function create_config( )
     "plugins",
     "tools"
  },
-    sudo_users = {167419742},
+    sudo_users = {456218820},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
